8f212062f74d2b54aed466e1a07091ae9e9a8bab
dasha2893 <missis.Bobiakowa@yandex.ru>
2015-04-02 23:33:47 +0300

    delete

--- sqls/data/product_types_parameters_mapping.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO public.product_types_parameters_mapping (product_type_id, parameter_id) VALUES (1, 1);
-INSERT INTO public.product_types_parameters_mapping (product_type_id, parameter_id) VALUES (2, 1);
-INSERT INTO public.product_types_parameters_mapping (product_type_id, parameter_id) VALUES (1, 2);
-INSERT INTO public.product_types_parameters_mapping (product_type_id, parameter_id) VALUES (2, 3);
-INSERT INTO public.product_types_parameters_mapping (product_type_id, parameter_id) VALUES (2, 2);
--- sqls/data/units.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO public.units (id, name, description) VALUES (2, 'Mb', 'мегабайты');
-INSERT INTO public.units (id, name, description) VALUES (1, 'GHz', 'Гигагерцы');
-INSERT INTO public.units (id, name, description) VALUES (3, 'Inch', 'дюймы');
